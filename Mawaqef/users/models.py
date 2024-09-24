c965c05f574222ea40b96a80ce8f791041f79e3f
Ziyad <Ziyad.M.Alshareef@gmail.com>
2024-09-24 23:06:01 +0300

    fixed DB hopefully

--- Mawaqef/users/models.py
+++ Mawaqef/users/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+# Model for Admin
+class Admin(models.Model):
+    email = models.EmailField(max_length=255, unique=True)  # Ensures email is unique
+    password = models.CharField(max_length=255)  # Password field
+
+    def __str__(self):
+        return self.email
+
+# Model for Operator
+class Operator(models.Model):
+    organization = models.CharField(max_length=255)  # Organization name
+    email = models.EmailField(max_length=255, unique=True)  # Ensures email is unique
+    password = models.CharField(max_length=255)  # Password field
+    phone_number = models.CharField(max_length=15)  # Phone number field
+    authorized = models.BooleanField(default=False)  # Boolean field for authorization status
+    photo = models.ImageField(upload_to='operators_photos/', blank=True, null=True)  # Field to upload operator's photo
+
+    def __str__(self):
+        return f'{self.organization} - {self.email}'
