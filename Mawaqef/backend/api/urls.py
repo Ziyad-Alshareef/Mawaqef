93c8e2ca108072e878425159d6d567c14def3ee3
Ali Alhoshan <143254937+A-Alhoshan@users.noreply.github.com>
2024-10-07 18:28:10 +0300

    Update urls.py

--- Mawaqef/backend/api/urls.py
+++ Mawaqef/backend/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import UnapprovedOperatorsView, AuthorizeOperatorView,UserDetailView, RejectOperatorView
+from .views import UnapprovedOperatorsView, AuthorizeOperatorView,UserDetailView, RejectOperatorView, AllAuthorizedOperatorsView
 
 urlpatterns = [
     path("notes/", views.NoteListCreate.as_view(), name="note-list"),
     path("notes/delete/<int:pk>/", views.NoteDelete.as_view(), name="delete-note"),
     path('operators/unapproved/', UnapprovedOperatorsView.as_view(), name='unapproved_operators'),
+    path('operators/authorized/', AllAuthorizedOperatorsView.as_view(), name='all_authorized_operators'),
     path('operators/<int:operator_id>/authorize/', AuthorizeOperatorView.as_view(), name='authorize_operator'),
     path('operators/<int:operator_id>/Reject/', RejectOperatorView.as_view(), name='reject_operator'),
     path('user/', UserDetailView.as_view(), name='user_detail'),
